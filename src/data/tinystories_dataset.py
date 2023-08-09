0624738bb98097ef7b0e6f5ebd516afe91c15237
sri9s <sri9s@yahoo.in>
2023-08-09 19:08:25 +0000

    Add pytorch iterabledataset

--- src/data/tinystories_dataset.py
+++ src/data/tinystories_dataset.py
@@ -3,6 +3,7 @@
 import argparse
 import glob
 import json
+import math
 import os
 import random
 import tarfile
@@ -12,7 +13,7 @@ import numpy as np
 import requests
 import torch
 import torch.distributed as dist
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, IterableDataset
 from tqdm import tqdm
 
 from src.data.tokenizer import Tokenizer
@@ -129,21 +130,11 @@ class PretokDataset(torch.utils.data.IterableDataset):
     """Loads pretokenized examples from disk and yields them as PyTorch tensors."""
 
     def __init__(self, split, max_seq_len):
-        """Initialize the dataset.
-
-        Parameters
-        ----------
-        split
-            Either "train" or "test".
-        max_seq_len
-            Maximum sequence length. Longer sequences will be truncated.
-        """
         super().__init__()
         self.split = split
         self.max_seq_len = max_seq_len
 
     def __iter__(self):
-        """Iterate over the dataset and yield batches of examples."""
         # get worker info within a DataLoader
         worker_info = torch.utils.data.get_worker_info()
         worker_id = worker_info.id if worker_info else 0
@@ -156,7 +147,7 @@ class PretokDataset(torch.utils.data.IterableDataset):
         data_dir = os.path.join(DATA_CACHE_DIR, "TinyStories_all_data")
         shard_filenames = sorted(glob.glob(os.path.join(data_dir, "*.bin")))
         # train/test split. let's use only shard 0 for test split, rest train
-        shard_filenames = shard_filenames[1:] if self.split == "train" else shard_filenames[:1]
+        shard_filenames = shard_filenames[1:5] if self.split == "train" else shard_filenames[:1]
         while True:
             rng.shuffle(shard_filenames)
             for shard in shard_filenames:
@@ -175,83 +166,6 @@ class PretokDataset(torch.utils.data.IterableDataset):
                     x = chunk[:-1]
                     y = chunk[1:]
                     yield x, y
-
-
-class Task:
-    """Task class for the TinyStories dataset."""
-
-    @staticmethod
-    def iter_batches(split, batch_size, max_seq_len, device, num_workers=0):
-        """Iterate batches of examples from the dataset.
-
-        Parameters
-        ----------
-        split
-            Either "train" or "test".
-        batch_size
-            Batch size.
-        max_seq_len
-            Maximum sequence length. Longer sequences will be truncated.
-        device
-            PyTorch device.
-        num_workers, optional
-            Number of workers for the DataLoader
-        """
-        ds = PretokDataset(split, max_seq_len)
-        dl = torch.utils.data.DataLoader(
-            ds, batch_size=batch_size, pin_memory=True, num_workers=num_workers
-        )
-        for x, y in dl:
-            x = x.to(device, non_blocking=True)
-            y = y.to(device, non_blocking=True)
-            yield x, y
-
-
-class TinyStoriesDataset(Dataset):
-    """Dataset class for the TinyStories dataset."""
-
-    def __init__(self, split, max_seq_len):
-        """Initialize the dataset."""
-        self.split = split
-        self.max_seq_len = max_seq_len
-        self.data_dir = "/home/jayachs1/tinystories-language-models/data/TinyStories_all_data"
-        self.shard_filenames = sorted(glob.glob(os.path.join(self.data_dir, "*.bin")))
-        print(f"Found {len(self.shard_filenames)} shards.")
-        self.shard_filenames = (
-            self.shard_filenames[1:] if self.split == "train" else [self.shard_filenames[0]]
-        )
-        self.data = []
-        print(self.shard_filenames)
-        rng = random.Random(9)
-        for shard in self.shard_filenames:
-            data_shard = np.memmap(shard, dtype=np.uint16, mode="r")
-            num_batches = len(data_shard) // self.max_seq_len
-            num_batches -= 1  # drop the last partial batch
-            assert num_batches > 0, "this shard is way too small? investigate."
-            ixs = list(range(num_batches))
-            rng.shuffle(ixs)
-            for ix in ixs:
-                start = ix * self.max_seq_len
-                end = start + self.max_seq_len + 1
-                # calling .astype will copy the data into a new numpy array, now in RAM
-                chunk = torch.from_numpy((data_shard[start:end]).astype(np.int64))
-                x = chunk[:-1]
-                y = chunk[1:]
-                # x = x.permute(1,0)
-                # y = y.permute(1,0)
-                self.data.append((x, y))
-        print(f"Loaded {len(self.data)} examples from {len(self.shard_filenames)} shards")
-
-    def __len__(self):
-        # log.info(f"data len- {len(self.data)}")
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        # log.info(f"idx- {idx}")
-        x, y = self.data[idx]
-        # log.info(f"x- {x.shape} y- {y.shape}")
-        # log.info(f"max x- {torch.max(x)} max y- {torch.max(y)}")
-        return x, y
 
 
 if __name__ == "__main__":
